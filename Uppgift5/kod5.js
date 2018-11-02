7bd56f77e28b317d29aecfd1439cea75449fc5b8
fridaschoultz <fridaschoultz@hotmail.com>
2018-11-02 14:28:05 +0100

    Ändringar i uppgiter

---
+++ Uppgift5/kod5.js
@@ -0,0 +1,62 @@
+// Skapa funktionen randomDice
+function randomDice(){
+			let r = []; //skapa array r som innehåller tomma index
+			let i=0; // skapa variabel i som är = 0
+				while(i<1000){ /* Så länge i<1000 ska satserna innanför while-satsen upprepas
+					            sen returnerar r tillbaks till arrayen */
+					
+					r[i] = [Math.floor(Math.random() * 6 + 1)]; /*I index "i" som ligger i array "r" 
+					                                             ska ett slumpmässig tal mellan 1-6 lagras*/
+					i++; //Öka "i" med ett, så nästa varv är i = 1 istället för 0.
+				}
+			 return r; // returnerar ett fält med slumpmässiga tal till variabeln r
+
+			}
+
+		function print(){   // skapar funktionen print(). En frekevenstabell.
+			let tal = randomDice(); /* Placerar randomDice som returnerar ett fält innehållande massa slumpmässiga tal 
+			                         till och med 1000*/
+			let ett = 0;             // variablerna ett-sex är lika med noll
+			let två = 0;
+			let tre = 0;
+			let fyra = 0;
+			let fem = 0;
+			let sex = 0;
+			i=0;                // tal[0] = tar ett slumpmässigt tal. Kollar sedan i if-sats efter korresponderande if-sats nedan
+			while(i<tal.length){ 
+				if(tal[i] == 1){ // Om indexen 'i' i fältet tal är lika med 1 så ska satserna i denna if-sats utföras, osv nedåt..
+					ett++;
+					i++;
+				}
+				if(tal[i] == 2){ 
+					två++;
+					i++;
+				}
+				if(tal[i] == 3){ 
+					tre++;
+					i++;
+				}
+				if(tal[i] == 4){ 
+					fyra++;
+					i++;
+				}
+				if(tal[i] == 5){ 
+					fem++;
+					i++;
+				}
+				if(tal[i] == 6){ 
+					sex++;
+					i++;
+				}
+			}
+			
+
+			// Visar respektive variabel och dess resultat i konsolen
+			console.log("Förekomsten av ettor: " + ett + "\nFörekomsten av tvåor: " + två + "\nFörekomsten av treor: " + 
+			tre + "\nFörekomsten av fyror: " + fyra + "\nFörekomsten av femor: " + fem + "\nFörekomsten av sexor: " + sex);
+
+			
+		}
+		print(); // anropa funktionen som utför frekvenstabellen
+ 
+
---
+++ Uppgift6/Kod6.js
@@ -0,0 +1,19 @@
+
+    function fakultet() { // skapat en fuktion som innehåller en lokal variabel tal
+        let tal = prompt("Skriv in talet"); // skriver in talet 
+        if(tal<=1000){ // if villkor och så länge detta stämmer så utförs for-loopen
+            for(x=1, fak=1; x<=tal; x++){ /* lokal variabel x och fak, så länge x<=tal så upprepas for-satsen
+                                            och x ökar med ett varje gång*/
+                fak *= x;             /* beräknar 1*1 och sedan ökar x med ett så att det i nästa
+                                       varv är 2 * 1, 3*1 osv, skulle kunna stå fak = x *fak */
+            }
+            return document.write(tal + "! = " + fak); // när satsen över stämmer så returneras ett svar till anvävndaren
+        }
+        else{
+            document.write("Talet får inte vara större än 1000!"); // annars så visas ett felmedelande
+        }
+    }
+
+    fakultet(); // anropar funktionen fakultet      
+
+
